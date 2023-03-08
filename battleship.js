979a0a83cce6e4392864f09ccaffe3f5216206ab
Janine <jlyn@cfa.harvard.edu>
2023-03-07 19:55:49 -0500

    Added in-line comments to classes, methods, and properties

--- battleship.js
+++ battleship.js
@@ -1,4 +1,4 @@
-
+// JSON object
 const placement = {
 	"ships":
 	[
@@ -36,11 +36,14 @@ function loadBoard() {
 	game.placeShips();
 }
 
+// Game class - builds the game board, places the ships according to the coordinates in the JSON object,
+// and keeps track of number of guesses
 function Game() {
 	this.boardDim = 6; 	// dimension for square board
-	this.numGuesses = 20;
-	this.squares = [];  // stores if square is a ship; if it is a ship, stores if it has been hit
+	this.numGuesses = 20;  //  number of guesses left
+	this.squares = [];  // stores an array of Square objects
 	this.shipCoords = [];  // array of square indicies where the ships are located
+	this.red = "#B22222";
 	this.initAnnounce = initAnnounce;
 	this.createButton = createButton;
 	this.buildBoard = buildBoard;
@@ -53,11 +56,15 @@ function Game() {
 	this.addRestartButton = addRestartButton;
 }
 
+// lets the user know how to start playing the game and the number of guesses they initially have
 function initAnnounce() {
 	let banner = document.getElementById("banner");
 	banner.innerHTML = "Click on the squares to play" + "</br>You have " + this.numGuesses + " guesses to sink 4 battleships";
 }
 
+// creates the HTML game board with buttons and underlying Square objects
+// Squares are the internal representation of the buttons and are used to keep track of if the button/square
+// is a ship and if a ship has been hit
 function buildBoard() {
 	let board = document.getElementById("board");
 	let grid = document.createElement('div');
@@ -72,6 +79,7 @@ function buildBoard() {
 	board.appendChild(grid);
 }
 
+// create a button and attach it to the grid
 function createButton(numId, grid) {
 
 	let button = document.createElement( "button");
@@ -87,12 +95,17 @@ function createButton(numId, grid) {
 	grid.appendChild(button);
 }
 
+// if a button has been clicked check to see if it is a ship
+// if the square is not a ship, it is a "miss"; change button to silver
+// if the square is a ship, it is a "hit"; change button to red
+// if all the ship squares have been hit, it is "sunk"; change all ship buttons to black
 function isClicked(id) {
 	let pos = parseInt(id.substring(6));
 	let btn = document.getElementById(id);
 
-	let action;
+	let resultStr;
 	let allSunk = false;
+	
 	//change sunken ship Squares to black
 	if (this.squares[pos].isShip === true) {
 		this.squares[pos].isHit = true;
@@ -101,38 +114,45 @@ function isClicked(id) {
 		let posArr = this.checkSunk(pos);
 
 		if (posArr != null) {
+			// if the entire ship has been hit, change ship buttons to black
 			for (let ii = 0; ii < posArr.length; ii++) {
 				document.getElementById("button" + posArr[ii]).style.backgroundColor = 'black';
 			}
+
+			// if all the ships squares have been hit, the game is over
 			allSunk = this.checkAllSunk();
 			if (allSunk === true) {
-				action = "Congratulations!  You sunk all the battleships!</br>GAME OVER";
+				resultStr = "Congratulations!  You sunk all the battleships!</br>GAME OVER";
 				this.numGuesses = 1;
 
 			} else {
-				action = "<b>!! SUNK !!</b>";
+				resultStr = "<b>!! SUNK !!</b>";
 			}
 
 		} else {
-			btn.style.backgroundColor = "#B22222";
-			action = "<b> >> HIT << </b>";
+			btn.style.backgroundColor = this.red;
+			resultStr = "<b> >> HIT << </b>";
 		}
 	} else {
 			btn.style.backgroundColor = 'silver';
-			action = "- MISS -";
+			resultStr = "- MISS -";
 	}
 
+	// disable button once it has been clicked
 	btn.disabled = true;
 
 	// update number of guesses
 	this.numGuesses--;
 	let banner = document.getElementById("banner");
+
+	// if the user has used all their guesses, the game is over
+	// reveal where the ships are located on the game board
 	if (this.numGuesses === 0) {
 		if (allSunk === false) {
 			banner.innerHTML = "GAME OVER </br> Here are all the ships";
 			this.showShips();
 		} else {
-			banner.innerHTML = action;
+			banner.innerHTML = resultStr;
 		}
 
 		// turn off buttons to prevent user from clicking more squares
@@ -142,10 +162,12 @@ function isClicked(id) {
 		this.addRestartButton();
 
 	} else {
-		banner.innerHTML = action + "<br/>You have " + this.numGuesses + " guesses left";
+		// announce the number of guesses left to the user
+		banner.innerHTML = resultStr + "<br/>You have " + this.numGuesses + " guesses left";
 	}
 }
 
+// loop through the squares;  if all ship squares have been hit, return true
 function checkAllSunk() {
 	let result = true;
 	this.squares.forEach( sq => {
@@ -156,10 +178,13 @@ function checkAllSunk() {
 
 	return result;
 }
+
+// find which ship was hit and check all the ship squares to see if it has been sunk
+// return the coordinates for the sunk ship
 function checkSunk(pos){
 	let result = null;
 
-	this.shipCoords.map( posArr => {
+	this.shipCoords.forEach( posArr => {
 		if (posArr.includes(pos)) {
 			let allHit = true;
 			posArr.forEach(sqIndex => {
@@ -174,9 +199,12 @@ function checkSunk(pos){
 
 	return result;
 }
+
+
+// using the JSON object, find the coordinates, length, and orientation of each ship and
+// if the Square coordinates match, update the Square.isShip to true
+// save the ship indices in an array
 function placeShips() {
-	// using the JSON object, find the coordinates, length, and orientation of each ship and
-	// if the Square coordinates match, update the Square.isShip to true
 
 	let index, startPos;
 	placement.ships.forEach( ship =>
@@ -199,6 +227,7 @@ function placeShips() {
 	)
 }
 
+// turn off hover and disable all buttons on the game board after the game has ended
 function disableAllButtons() {
 	for (let ii = 0; ii < Math.pow(this.boardDim,2); ii++) {
         let btn = document.getElementById("button" + ii);
@@ -207,6 +236,9 @@ function disableAllButtons() {
     }
 
 }
+
+// after the game has ended, add a button below the
+// game board allowing the user to reset the board and play another game
 function addRestartButton() {
     let restart = document.getElementById("restart");
     let btn = document.createElement( "button");
@@ -219,6 +251,7 @@ function addRestartButton() {
     restart.appendChild(btn);
 }
 
+// reload board and start a new game
 function reloadBoard() {
     // remove the old board
     let div = document.getElementById("grid");
@@ -231,11 +264,13 @@ function reloadBoard() {
     // load a fresh game board
     loadBoard();
 }
+
+// at the end of the game, update the game board to show where the ships are located
 function showShips() {
 	let index = 0;
 	this.squares.forEach( sq => {
 		if  (sq.isShip === true) {
-			document.getElementById("button" + index).style.backgroundColor = "#B22222";
+			document.getElementById("button" + index).style.backgroundColor = this.red;
 		} else {
 			document.getElementById("button" + index).style.backgroundColor = "lightgray";
 		}
@@ -244,6 +279,7 @@ function showShips() {
 }
 
 
+// Square class - stores info on whether the square/button is a ship and if the ship has been hit
 function Square() {
 	this.isShip = false;
 	this.isHit = false;
